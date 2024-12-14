fd5ba01aad440346962acffc1452735d9c3a2a19
Arthur Almeida <arthuralmeidaa66@gmail.com>
2024-12-13 21:33:24 -0300

    update

---
+++ client/build/precache-manifest.56bcc0dc493139abdb89a02331979ce7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b639f7ec29756fc7ffb8976c7fa237cb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01e3b718119ad1a8edb3",
+    "url": "./static/css/main.f61b4099.chunk.css"
+  },
+  {
+    "revision": "aa5b24012d9a5173e274",
+    "url": "./static/js/2.d3b21c9f.chunk.js"
+  },
+  {
+    "revision": "8e3a127b3eb410b6d841",
+    "url": "./static/js/3.445915d0.chunk.js"
+  },
+  {
+    "revision": "01e3b718119ad1a8edb3",
+    "url": "./static/js/main.a5b995b7.chunk.js"
+  },
+  {
+    "revision": "9b3fcb02472347db9a07",
+    "url": "./static/js/runtime~main.a8664220.js"
+  }
+]);
