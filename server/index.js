19c52a1f3c241480efebcee9ab73607166caf040
Arthur Almeida <arthuralmeidaa66@gmail.com>
2024-12-17 13:55:06 -0300

    update

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const PORT = 3001;
 app.use(express.static(path.join(__dirname, "build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
+  return
 });
 
 
